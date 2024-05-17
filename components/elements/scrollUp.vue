9bac479fd8ea291d6480194a2d493414e9033dc1
Demi van der Maarl <112861166+Demivdm@users.noreply.github.com>
2024-05-17 15:06:01 +0200

    adds scroll up button

--- components/elements/scrollUp.vue
+++ components/elements/scrollUp.vue
@@ -1,5 +1,40 @@
 <template>
-   <button>
-    scroll up
-   </button>
+   <div class="wrapper">
+      <button v-if="isVisible" @click="scrollToTop" class="scroll-up-button">scroll up</button>
+
+   </div>
+
 </template>
+<script setup>
+
+const isVisible = ref(false);
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+};
+
+const handleScroll = () => {
+  isVisible.value = window.scrollY > 300; // Adjust the value as needed
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
+</script>
+<style>
+.wrapper{
+   display: flex;
+   justify-content: flex-end;
+}
+.scroll-up-button {
+  position: fixed;
+
+}
+</style>
