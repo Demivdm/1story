f80baf5f657c48f18603110049bab7cfa71b546c
Demi van der Maarl <112861166+Demivdm@users.noreply.github.com>
2024-05-21 16:17:52 +0200

    adjusted static text

--- pages/loginPage.vue
+++ pages/loginPage.vue
@@ -1,16 +1,22 @@
 <template>
     <BlocksNav></BlocksNav>
-    <ElementsLogin></ElementsLogin>
-    <ElementsBackButton></ElementsBackButton>
-    <span v-if="currentUser">
+
+    <span v-if="!currentUser">
+        <ElementsLogin></ElementsLogin>
+      </span>
+    <span v-else-if="currentUser">
         <h2>
-        Welcome {{ currentUser.displayName }}
+        Deel jouw zin van de week {{ currentUser.displayName }}
         </h2>
-        <p>Je bent nu ingelogd, en kunt een zin schrijven door op de onderstaande knop te klikken.</p>
- 
+        <p>Je bent nu ingelogd, er is een begin gemaakt aan het verhaal van de maand. Deel nu jouw zin met je collega's om het verhaal voort te zetten.</p>
     
       <ElementsButton to="/writeStory">Deel je zin</ElementsButton>
+      <ElementsButton to="/allStories">Bekijk alle verhalen</ElementsButton>
       </span>
+      <span v-else>
+        <p>Oeps er lijkt iets mis te zijn gegaan</p>
+      </span>
+     
 </template>
 <script setup>
   const currentUser = useCurrentUser();
